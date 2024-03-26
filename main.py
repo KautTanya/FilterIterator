7c9407fc5e471896d89bd1776197a63b916e85ae
Kaut Tanya <kaut.tanya.s@gmail.com>
2024-03-26 17:27:14 +0200

    Add main file

---
+++ main.py
@@ -0,0 +1,25 @@
+"""FilterIterator"""
+
+
+class FilterIterator:
+    """FilterIterator"""
+    def __init__(self, obj, func):
+        self.obj = iter(obj)
+        self.func = func
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            value = next(self.obj)
+            if self.func(value):
+                return value
+
+
+f_iter = FilterIterator([1, 2, 3, 4], lambda x: x % 2 == 0)
+while True:
+    try:
+        print(next(f_iter))
+    except StopIteration:
+        break
